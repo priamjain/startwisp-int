f4f5018405e1f68ae8763e78b4e23a2ffca4887b
Priam Jain <priamjain8@gmail.com>
2020-11-28 01:52:06 +0530

    First Page Complete

--- src/Components/Notepad.jsx
+++ src/Components/Notepad.jsx
@@ -1,26 +1,45 @@
 import {useState} from 'react'
 import Styles from './Notepad.module.css'
 let Notepad = () =>{
-
+	const [list,setList] = useState([]);
+	const [title, setTitle] = useState("")
+	const [note, setNote] = useState("")
+	const [np,setNp] = useState(true);
+	let addToArray = () =>{
+		if(np===true){
+			setNp(inp=>!inp)
+		}
+		else{
+		setList([...list,{title,note}]);
+		setTitle("");
+		setNote("");
+		setNp(inp=>!inp)
+		}
+	}
 	return(
 		<div className={Styles.main}>
 			<div className={Styles.head}>Notepad</div>
-			<div className={Styles.list}>
-				<div className={Styles.list_element}>
-					<span className={Styles.element_body}>title</span>
-					<span className={Styles.element_body} style={{color:"#D0D0D0"}}>parah</span>
-				</div>
-				<div className={Styles.list_element}>
-					<span className={Styles.element_body}>title</span>
-					<span className={Styles.element_body} style={{color:"#D0D0D0"}}>parah</span>
-				</div>
-				<div className={Styles.list_element}>
-					<span className={Styles.element_body}>title</span>
-					<span className={Styles.element_body} style={{color:"#D0D0D0"}}>parah</span>
-				</div>
+			<div className={`${Styles.list} ${np?(Styles.show):(Styles.hide)}`}>
+				{	
+					list.map((data,index)=>{
+						return(
+					<div key={index} className={Styles.list_element}>
+									<span className={Styles.element_body}>{data.title}</span>
+									<span className={Styles.element_body} style={{color:"#D0D0D0"}}>{data.note}</span>
+					</div>)
+							})
+				}
+				
+			</div>
+			<div className={`${Styles.input_form} ${!np?(Styles.show):(Styles.hide)}`}>
+				<div className={Styles.element_body}>Title</div>
+				<input className={`${Styles.input_title} ${Styles.input}`} type="text" name="title" value={title} onChange={(e)=>setTitle(e.target.value)} id=""/>
+				<div className={Styles.element_body}>Note</div>
+				<textarea className={`${Styles.input}`} name="para" id="" cols="30" rows="10" value={note} onChange={(e)=>setNote(e.target.value)}></textarea>
+				<button onClick={(e)=>{setNp(inp=>!inp)}} className={`${Styles.sign} ${Styles.cross_sign} ${!np?(Styles.show):(Styles.hide)}`}>x</button>
 			</div>
 			<div>
-				<button className={Styles.add_sign}>+</button>
+				<button onClick={(e)=>{addToArray()}} className={`${Styles.sign} ${Styles.add_sign}`}>+</button>
 			</div>
 		</div>
 		)
